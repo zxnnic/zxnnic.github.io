9936d13ada25f8ef0a532105e787eb8d24d33ccb
zxnnic <zzhao1@ualberta.ca>
2025-10-17 01:06:07 -0400

    update site

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+// switch out to d3
+// thumbnail to youtube link in separate page
+// bubble thing on top of title
+// youtube video thumbnail in the highlighted ones
+// 
+
+
+
 const header_text = " zixin zhao zi xin zhao 赵 子 欣 z i x i n  z h a o zixin (nicole) zhao nicole (zixin) zhao zixin nicole zhao nicole zhao zixin zhao zi xin zhao 赵 子 欣 z i x i n  z h a o zixin (nicole) zhao nicole (zixin) zhao zixin nicole zhao nicole zhao zixin zhao zi xin zhao z i x i n  z h a o zixin (nicole) zhao nicole zhao zixin zhao zi xin zhao 赵 子 欣 z i x i n  z h a o zixin (nicole) zhao nicole (zixin) zhao zixin nicole zhao nicole zhao zixin zhao zi xin zhao z i x i n  z h a o zixin (nicole)";
 const header = d3.select("#header");
 const dur = 50
@@ -16,30 +24,63 @@ function typeHeader() {
 typeHeader();
 
 $(document).ready(() => {
-    console.log('woah')
     //////////////////////// Profile picture
-    // Swivel image on hover for circle
-    $('.circle').hover(
-        function() {
-            $(this).addClass('swivel');
-        },
-        function() {
-            $(this).removeClass('swivel');
-        }
-    );
+    // Fade front/back images on hover instead of swivel
+    $('.circle').each(function() {
+        const $c = $(this);
+        const $front = $c.find('.front');
+        const $back = $c.find('.back');
+        // ensure starting state
+        $front.css('opacity', 1);
+        $back.css('opacity', 0);
+        $c.on('mouseenter', function() {
+            $front.stop(true, true).fadeTo(200, 0);
+            $back.stop(true, true).fadeTo(200, 1);
+        }).on('mouseleave', function() {
+            $front.stop(true, true).fadeTo(200, 1);
+            $back.stop(true, true).fadeTo(200, 0);
+        });
+    });
 
     ////////////////////////////////// Show papers
-    d3.csv('papers.csv').then(function(data) {
-        const container = d3.select('#all-research');
-        const featuredContainer = d3.select('#featured-research');
+    // Create a lightweight modal for displaying Bibtex
+    if ($('#bib-modal').length === 0) {
+        $('body').append(`
+            <div id="bib-modal" style="display:none;">
+                <div class="bib-overlay"></div>
+                <div class="bib-box">
+                    <button class="bib-close" aria-label="Close">&times;</button>
+                    <pre class="bib-content"></pre>
+                </div>
+            </div>
+        `);
+    }
+
+    // Delegated handler for bib links
+    $(document).on('click', '.bib-link', function(e) {
+        e.preventDefault();
+        const encoded = $(this).attr('data-bib') || '';
+        const bib = decodeURIComponent(encoded);
+        $('#bib-modal .bib-content').text(bib);
+        $('#bib-modal').fadeIn(150);
+    });
+
+    // Close handlers
+    $(document).on('click', '#bib-modal .bib-close, #bib-modal .bib-overlay', function() {
+        $('#bib-modal').fadeOut(120);
+    });
+    $.get('papers.csv', function(csvString) {
+        const result = Papa.parse(csvString, { header: true });
+        const data = result.data;
+
         // Sort data in reverse order by id
         data.sort((a, b) => Number(b.id) - Number(a.id));
 
         // Populate featured-research with entries where feature=true (case-insensitive)
-        featuredContainer.html("<h5>Recent Works</h5>");
+        $('#featured-research').html("<h5>Recent Works</h5>");
         data.forEach(function(d) {
             if (d.feature && (d.feature === true || String(d.feature).toLowerCase() === "true")) {
-                insertDataCol(featuredContainer, d);
+                insertDataCol($('#featured-research'), d);
             }
         });
 
@@ -48,31 +89,30 @@ $(document).ready(() => {
 
 
         // Refactored filter logic using jQuery
-        $('.btn-conf').on('click', function() {
-            $('.btn-conf').removeClass('selected');
+        $('.btn-conf-filter').on('click', function() {
+            $('.btn-conf-filter').removeClass('selected');
             $(this).addClass('selected');
             if (this.id === 'filter-all') {
                 renderPapers();
             } else if (this.id === 'filter-nlp') {
-                renderPapers(['acl']);
+                renderPapers(['nlp']);
             } else if (this.id === 'filter-hci') {
-                renderPapers(['chi', 'uist', 'misc', 'cc']);
+                renderPapers(['hci']);
             } else if (this.id === 'filter-vis') {
-                renderPapers(['dis', 'vis']);
+                renderPapers(['vis']);
             }
         });
 
         // for all the papers
         function renderPapers(filterClasses) {
-            console.log(this)
-            container.selectAll('.paper-row').remove();
+            $('#all-research').find('.paper-row').remove();
             data.forEach(function(d) {
                 if (!filterClasses) {
-                    insertDataRow(container, d)
+                    insertDataRow($('#all-research'), d)
                 } else {
                     filterClasses.forEach((filterClass) => {
                         if (d.class.includes(filterClass)) {
-                            insertDataRow(container, d)
+                            insertDataRow($('#all-research'), d)
                         }
                     })
                 }
@@ -80,7 +120,7 @@ $(document).ready(() => {
         }
         // insert them one row at a time
         function insertDataRow(container, d) {
-            console.log(d)
+            if (!d.authors) return
             let authors = d.authors || "";
             authors = authors.replace(/Zixin Zhao/g, '<span style="text-decoration:underline;font-style:italic;">Zixin Zhao</span>')
                              .replace(/Nicole Zhao/g, '<span style="text-decoration:underline;font-style:italic;">Nicole Zhao</span>');
@@ -88,9 +128,12 @@ $(document).ready(() => {
             if (d.demo_link !== '') {
                 links += `<a href="${d.demo_link}" target="_blank">Project Page</a>&nbsp;|&nbsp;`;
             }
-            links += `<a href="${d.paper_link}" target="_blank">Paper</a>&nbsp;|&nbsp;
-                    <a href="${d.bibtext}" target="_blank">Bibtex</a>`;
-            container.append('div').html(`
+            // Clean bibtext: remove surrounding double quotes if present
+            const _bibRawCol = d.bibtext || '';
+            const _bibCleanCol = _bibRawCol.trim().replace(/^"/, '').replace(/"$/, '');
+            links += `<a href="${d.paper_link}" target="_blank">Paper</a>&nbsp;|&nbsp;<a href="#" class="bib-link" data-bib="${encodeURIComponent(_bibCleanCol)}">Bibtex</a>`;
+
+            const paperHtml = `
                 <div class="row ${d.class} my-2 paper-row" id="paper-${d.id}">
                     <div class="col-12 col-md-3 d-none d-md-block" style="max-width:150px;height:100%;">
                         <img src='${d.icon}' width="150px" 
@@ -100,63 +143,87 @@ $(document).ready(() => {
                     </div>
                     <div class="col-12 col-md-9">
                         <div class="row px-3">
-                            <btn class="btn-${d.class} ">${d.conference}</div>
-                            <div class="fw-bold px-1">${d.title}</div>
-                            <div class="px-1">${authors}</div>
-                            <div class="px-1 pt-2" style="font-size:small">${d.tldr}</div>
-                            <div class="px-1 my-2">
+                            <btn class="btn-${d.class} mx-2">${d.conference}</btn>
+                            <div class="fw-bold px-1 mx-2">${d.title}</div>
+                            <div class="px-1 mx-2">${authors}</div>
+                            <div class="px-1 pt-2 mx-2" style="font-size:small">${d.tldr}</div>
+                            <div class="px-1 my-2 mx-2">
                                 ${links}
                             </div>
                         </div>
                     </div>
                 </div>
-            `);
+            `;
+
+            container.append(paperHtml);
+
+            // Add image hover effect after appending
+            container.find(`#paper-${d.id} img`).hover(
+            function() {
+                if (d.icon_moving && d.icon_moving !== '') $(this).attr('src', d.icon_moving);
+            },
+            function() {
+                $(this).attr('src', d.icon);
+            }
+            );
+
         }
         // insert them vertically
         function insertDataCol(container, d) {
-            const div = document.createElement('div');
-            div.className = `col-12 col-md-4 ${d.class} my-3 paper-row`;
-            div.id = `paper-${d.id}`;
             let authors = d.authors || "";
             authors = authors.replace(/Zixin Zhao/g, '<span style="text-decoration:underline;font-style:italic;">Zixin Zhao</span>')
                              .replace(/Nicole Zhao/g, '<span style="text-decoration:underline;font-style:italic;">Nicole Zhao</span>');
-            let links = ''
+            let links = '';
             if (d.demo_link !== '') {
                 links += `<a href="${d.demo_link}" target="_blank">Project Page</a>&nbsp;|&nbsp;`;
             }
-            links += `<a href="${d.paper_link}" target="_blank">Paper</a>&nbsp;|&nbsp;
-                    <a href="${d.bibtext}" target="_blank">Bibtex</a>`;
-            div.innerHTML = `
-                <div class="text-center icon-img">
-                    <img src='${d.icon_moving}' style="height:100%;max-width:auto;object-fit:contain;"
-                        onmouseover="this.src='${d.icon_moving}'"
-                        onmouseout="this.src='${d.icon_moving}'"
-                    />
+            // Clean bibtext: remove surrounding double quotes if present
+            const _bibRawCol = d.bibtext || '';
+            const _bibCleanCol = _bibRawCol.trim().replace(/^"/, '').replace(/"$/, '');
+            links += `<a href="${d.paper_link}" target="_blank">Paper</a>&nbsp;|&nbsp;<a href="#" class="bib-link" data-bib="${encodeURIComponent(_bibCleanCol)}">Bibtex</a>`;
+
+            const paperHtml = `
+            <div class="col-12 col-md-4 ${d.class} my-3 paper-row" id="paper-${d.id}">
+                <div class="text-center icon-img" style="height:200px; padding:8px; display:flex; align-items:center; justify-content:center;">
+                <img src="${d.icon}" style="max-height:100%; max-width:200px; object-fit:contain;">
                 </div>
+                <div><btn class="btn-${d.class}">${d.conference}</btn></div>
                 <div class="fw-bold px-1">${d.title}</div>
                 <div class="px-1">${authors}</div>
                 <div class="px-1 pt-2" style="font-size:small">${d.tldr}</div>
                 <div class="px-1 my-1">${links}</div>
-                <div class="px-1">@${d.conference}</div>
+                
+            </div>
             `;
-            container.node().appendChild(div);
+
+            container.append(paperHtml);
+
+            // Add image hover effect after appending
+            container.find(`#paper-${d.id} img`).hover(
+            function() {
+                if (d.icon_moving && d.icon_moving !== '') $(this).attr('src', d.icon_moving);
+            },
+            function() {
+                $(this).attr('src', d.icon);
+            }
+            );
         }
     });
 
 
-    //////////////////////////////////////////////////////////
-    // Funky image
-    // List of random images
-    const randomImages = [
-        'dog1.gif', 
-        'no_money.gif',
-        'big_nose.gif',
-        'guinea_pig.gif'
-        // 'happy_holidays_snowflake_glitter.gif', 
-    ];
-    $('#surprise').on('click', function() {
-        const idx = Math.floor(Math.random() * randomImages.length);
-        const imgTag = `<img src='./images/random/${randomImages[idx]}' style='max-width:150px;height:100%;height:auto;'>`;
-        $('#surprise-image').html(imgTag);
-    });
+    // //////////////////////////////////////////////////////////
+    // // Funky image
+    // // List of random images
+    // const randomImages = [
+    //     'dog1.gif', 
+    //     'no_money.gif',
+    //     'big_nose.gif',
+    //     'guinea_pig.gif'
+    //     // 'happy_holidays_snowflake_glitter.gif', 
+    // ];
+    // $('#surprise').on('click', function() {
+    //     const idx = Math.floor(Math.random() * randomImages.length);
+    //     const imgTag = `<img src='./images/random/${randomImages[idx]}' style='max-width:150px;height:100%;height:auto;'>`;
+    //     $('#surprise-image').html(imgTag);
+    // });
 });
